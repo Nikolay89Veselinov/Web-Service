3091755beace5c146d05c7e9cf09c43b935cef43
Nikolay Veselinov <nikolay.veselinov@industria.tech>
2021-10-19 12:26:32 +0300

    Created a method that checks whether the number is in Fibonacci numbers

--- contrib/web_service/views.py
+++ contrib/web_service/views.py
@@ -1,3 +1,4 @@
+import math
 import json
 import requests
 
@@ -6,11 +7,34 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
+def check_is_fibonacci_number(number_of_terms):
+    counter = 0
+    num1 = 1
+    num2 = 1
+    is_fibonacci = False
+
+    if number_of_terms == 0 or number_of_terms == 1:
+        is_fibonacci = True
+    else:
+        while counter < number_of_terms:
+            counter = num1 + num2
+            num2 = num1
+            num1 = counter
+        if counter == number_of_terms:
+            is_fibonacci = True
+    return is_fibonacci
+
+
+def findIndex(n):
+    fibo = 2.078087 * math.log(n) + 1.672276
+    return round(fibo)
+
 @csrf_exempt
 def return_response(request):
 
     data = request.GET
-    result = []
+    context = {}
+    result = 0
 
     if isinstance(data, dict) and len(data) > 0:
         x = int(data.get('x', False))
@@ -26,5 +50,11 @@ def return_response(request):
         elif operation == '/':
             result = x / y
 
-    json_content = json.dumps(result).encode('utf-8')
+    context.update({
+        'result': result,
+        'is_fibonacci': check_is_fibonacci_number(result),
+        'counter': findIndex(result),
+    })
+
+    json_content = json.dumps(context).encode('utf-8')
     return HttpResponse(json_content, 'application/json; charset=utf-8')
